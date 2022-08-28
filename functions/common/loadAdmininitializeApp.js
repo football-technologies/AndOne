f7467d31595bfc9a4659bc1e2b46a6cc5dc91fe8
imoto2000 <imoto.morefun@gmail.com>
2022-08-29 08:53:32 +0900

    install firebase-tools

--- functions/common/loadAdmininitializeApp.js
+++ functions/common/loadAdmininitializeApp.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
-const serviceAccount = functions.config().service_account;
+// const serviceAccount = functions.config().service_account;
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    // credential: admin.credential.cert(serviceAccount),
+    credential: applicationDefault(),
     databaseURL: JSON.parse(process.env.FIREBASE_CONFIG).databaseURL,
     storageBucket: JSON.parse(process.env.FIREBASE_CONFIG).storageBucket,
   });
