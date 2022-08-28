6cced0d84f4945f222b4ddda25babccd05cced58
imoto2000 <imoto.morefun@gmail.com>
2022-08-29 07:13:21 +0900

    modi 環境変数の読み込み

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const functions = require("firebase-functions");
+let CURRENT_ENV = process.env.NODE_ENV;
+if (functions.config().basic) {
+  CURRENT_ENV = functions.config().basic.env;
+} else {
+  CURRENT_ENV = process.env.NODE_ENV;
+}
+
+console.log(">>>>>>>  CURRENT_ENV", CURRENT_ENV);
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
