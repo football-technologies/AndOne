366baef73900d534310ecb58369a48f288dbc9b5
imoto2000 <imoto.morefun@gmail.com>
2022-08-29 09:25:12 +0900

    install firebase-tools

--- functions/common/nextapp.js
+++ functions/common/nextapp.js
@@ -1,6 +1,4 @@
 const functions = require("firebase-functions");
-// const express = require("express");
-// const basicAuth = require("basic-auth-connect");
 const { Next } = require("next");
 const nextConfig = require("../next.config.js");
 
@@ -11,33 +9,12 @@ const config = {
   buildDir: ".next",
 };
 const next = new Next(config);
-// const app = express();
 
 const runtimeOpts = {
   timeoutSeconds: 540,
   memory: "8GB",
 };
 
-// stagingには、basic認証が必要
-// if (functions.config().basic.env === "staging") {
-//   const USERNAME = functions.config().basic_auth.name;
-//   const PASSWORD = functions.config().basic_auth.password;
-//   app.use(basicAuth(USERNAME, PASSWORD));
-//   app.use(async (req, res) => {
-//     await next.ready();
-//     res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-//     next.render(req, res);
-//   });
-//   module.exports = functions.runWith(runtimeOpts).https.onRequest(app);
-// } else {
-//   module.exports = functions
-//     .runWith(runtimeOpts)
-//     .https.onRequest(async (req, res) => {
-//       await next.ready();
-//       res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-//       next.render(req, res);
-//     });
-// }
 module.exports = functions
   .runWith(runtimeOpts)
   .https.onRequest(async (req, res) => {
