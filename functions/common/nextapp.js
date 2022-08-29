6b4a46596afe15ecc13d9401b4faf0d5bcdc49f5
imoto2000 <imoto.morefun@gmail.com>
2022-08-29 09:24:09 +0900

    install firebase-tools

--- functions/common/nextapp.js
+++ functions/common/nextapp.js
@@ -1,16 +1,16 @@
 const functions = require("firebase-functions");
 // const express = require("express");
 // const basicAuth = require("basic-auth-connect");
-const { Nuxt } = require("nuxt");
-const nuxtConfig = require("../nuxt.config.js");
+const { Next } = require("next");
+const nextConfig = require("../next.config.js");
 
 const config = {
-  ...nuxtConfig,
+  ...nextConfig,
   dev: false,
   debug: false,
-  buildDir: ".nuxt",
+  buildDir: ".next",
 };
-const nuxt = new Nuxt(config);
+const next = new Next(config);
 // const app = express();
 
 const runtimeOpts = {
@@ -24,24 +24,24 @@ const runtimeOpts = {
 //   const PASSWORD = functions.config().basic_auth.password;
 //   app.use(basicAuth(USERNAME, PASSWORD));
 //   app.use(async (req, res) => {
-//     await nuxt.ready();
+//     await next.ready();
 //     res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-//     nuxt.render(req, res);
+//     next.render(req, res);
 //   });
 //   module.exports = functions.runWith(runtimeOpts).https.onRequest(app);
 // } else {
 //   module.exports = functions
 //     .runWith(runtimeOpts)
 //     .https.onRequest(async (req, res) => {
-//       await nuxt.ready();
+//       await next.ready();
 //       res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-//       nuxt.render(req, res);
+//       next.render(req, res);
 //     });
 // }
 module.exports = functions
   .runWith(runtimeOpts)
   .https.onRequest(async (req, res) => {
-    await nuxt.ready();
+    await next.ready();
     res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-    nuxt.render(req, res);
+    next.render(req, res);
   });
