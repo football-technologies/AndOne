fc714aaa5d4f7389ce7105ee3d0cc6be1716751b
imoto2000 <imoto.morefun@gmail.com>
2022-08-29 10:02:44 +0900

    modi nextapp

--- functions/common/nextapp.js
+++ functions/common/nextapp.js
@@ -1,24 +1,38 @@
 const functions = require("firebase-functions");
-const { Next } = require("next");
+const { default: next } = require("next");
 const nextConfig = require("../next.config.js");
 
-const config = {
+const nextjsServer = next({
   ...nextConfig,
   dev: false,
-  debug: false,
-  buildDir: ".next",
-};
-const next = new Next(config);
+  conf: {
+    distDir: ".next",
+  },
+});
+const nextjsHandle = nextjsServer.getRequestHandler();
 
-const runtimeOpts = {
-  timeoutSeconds: 540,
-  memory: "8GB",
-};
+exports.nextjsFunc = https.onRequest((req, res) => {
+  return nextjsServer.prepare().then(() => nextjsHandle(req, res));
+});
 
-module.exports = functions
-  .runWith(runtimeOpts)
-  .https.onRequest(async (req, res) => {
-    await next.ready();
-    res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-    next.render(req, res);
-  });
+// const config = {
+//   ...nextConfig,
+//   dev: false,
+//   debug: false,
+//   buildDir: ".next",
+// };
+
+// const next = new Next(config);
+
+// const runtimeOpts = {
+//   timeoutSeconds: 540,
+//   memory: "8GB",
+// };
+
+// module.exports = functions
+//   .runWith(runtimeOpts)
+//   .https.onRequest(async (req, res) => {
+//     await next.ready();
+//     res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
+//     next.render(req, res);
+//   });
