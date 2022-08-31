0d8c9255093e71cf3c75a5a3aa321051134831c9
mino2022 <mino@football-technologies.work>
2022-08-31 12:13:23 +0900

    test useToast

--- src/pages/admin/dummy/index.js
+++ src/pages/admin/dummy/index.js
@@ -2,7 +2,12 @@ import { Button } from "@chakra-ui/react";
 import { functions } from "@/plugins/firebase";
 import { httpsCallable } from "firebase/functions";
 
+import { useToast } from "@chakra-ui/react";
+
 const DummyIndex = () => {
+  const toast = useToast();
+  const id = "test";
+
   const helloOnCall = async () => {
     const hello = httpsCallable(functions, "v1-callable-hello");
     console.log(">>>>>> Start");
@@ -10,7 +15,30 @@ const DummyIndex = () => {
     console.log(">>>>>> Return: ->", result);
   };
 
-  return <Button onClick={helloOnCall}>Hello On Call</Button>;
+  return (
+    <>
+      <Button
+        onClick={() => {
+          console.log(toast.isActive(id));
+          if (toast.isActive(id)) {
+            return false;
+          }
+          toast({
+            id: id,
+            position: "top",
+            title: "Account created.",
+            description: "We've created your account for you.",
+            status: "success",
+            duration: 2000,
+            isClosable: true,
+          });
+        }}
+      >
+        Show Toast
+      </Button>
+      <Button onClick={helloOnCall}>Hello On Call</Button>;
+    </>
+  );
 };
 
 export default DummyIndex;
