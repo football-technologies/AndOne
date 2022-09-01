8584c88d7e6324f06ee27f502271766640cd3d84
imoto2000 <imoto.morefun@gmail.com>
2022-09-01 15:55:37 +0900

    add google fonts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,17 +1,15 @@
 import SideNavBasic from "@/components/layouts/sidenav/SideNavBasic";
-import { HStack, Stack } from "@chakra-ui/react";
 
 const DefaultLayout = ({ children }) => {
   return (
     <>
-      <HStack>
-        <Stack w="30%" h="100vh" bgColor="white">
-          <SideNavBasic></SideNavBasic>
-        </Stack>
-        <Stack w="70%" h="100vh" bgColor="gray.300">
-          <main>{children}</main>
-        </Stack>
-      </HStack>
+      <header id="ftHeader">
+        <SideNavBasic></SideNavBasic>
+      </header>
+
+      <article id="ftMain">
+        <main id="ftMainContainer">{children}</main>
+      </article>
     </>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { getDoc, doc } from "firebase/firestore";
 
 import { useState, useEffect } from "react";
 
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 import { ToFullDate, ToAgo } from "@/plugins/filter";
 
@@ -23,6 +23,10 @@ export default function Home() {
       <Box bg="black">aaa</Box>
       {dummy && (
         <>
+          <Text>
+            最近はスマートフォンからWebサイトを閲覧するユーザーが増え、制作におけるスマートフォン対策は必須となりました。そのため、レスポンシブWebデザインを導入することも多くなり、HTML5とCSS3でのWebサイト制作の需要が高まっています。
+            今回は、最新のWebサイト制作に欠かせないHTML5・CSS3とはなにか、制作に必要な習得スキルにはどんなものがあるかを考えてみます。
+          </Text>
           <Button>name: {dummy.name}</Button>
           <h3>createdAt: {ToFullDate(dummy.createdAt)}</h3>
           <h3>toAgo: {ToAgo(dummy.createdAt)}</h3>
