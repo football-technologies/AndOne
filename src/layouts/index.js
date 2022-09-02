b2b3b3db6114f93c9f9489f640d2cda22573c00a
mino2022 <mino@football-technologies.work>
2022-09-02 11:40:37 +0900

    modi Authentication

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,37 @@
 import SideNavBasic from "@/components/layouts/sidenav/SideNavBasic";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { auth } from "@/plugins/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { login } from "@/store/account";
 
 const DefaultLayout = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log(">>>>>>>>> login user", user);
+
+        await user.reload();
+
+        // const userToken = await user.getIdTokenResult(true);
+        // const claims = userToken.claims;
+
+        dispatch(
+          login({
+            // id: claims.userId,
+            email: user.email,
+            name: user.displayName,
+          })
+        );
+      } else {
+        console.log(">>>>>>>>>> logout user");
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <>
       <header id="ftHeader">
