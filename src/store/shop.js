abac0fdb740bc2114c14d41f484d99391210c6b0
imoto2000 <imoto.morefun@gmail.com>
2022-09-06 16:34:20 +0900

    add /shops/index

--- src/store/shop.js
+++ src/store/shop.js
@@ -1,7 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 import { db } from "@/plugins/firebase";
-import { doc, setDoc, serverTimestamp, onSnapshot } from "firebase/firestore";
+import {
+  doc,
+  setDoc,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  collection,
+  orderBy,
+} from "firebase/firestore";
 
 const shop = createSlice({
   name: "shop",
@@ -23,13 +31,16 @@ const shop = createSlice({
     readShop(state, { type, payload }) {
       state.shop = { ...payload };
     },
+
+    readShops(state, { type, payload }) {
+      state.shops = [...payload];
+    },
   },
 });
 
 const fetchShop = (payload) => {
   // return async (dispatch, getState) => {
   //   console.log(">>>>>>>>> called fetchUser");
-
   //   const unsubscribe = await onSnapshot(
   //     doc(db, "users", payload.query),
   //     async (doc) => {
@@ -38,13 +49,57 @@ const fetchShop = (payload) => {
   //       }
   //     }
   //   );
-
   //   if (payload.type === "delete") {
   //     unsubscribe();
   //   }
   // };
 };
 
-export const { createShop, updateShop } = shop.actions;
-export { fetchShop };
+const fetchShops = (payload) => {
+  return async (dispatch, getState) => {
+    console.log(">>>>>>>>> called fetchUser");
+
+    const newShops = [];
+    const q = query(
+      collection(db, payload.query),
+      orderBy("createdAt", "desc")
+    );
+
+    const unsubscribe = await onSnapshot(q, async (snapshot) => {
+      if (snapshot) {
+        await snapshot.docChanges().forEach(async (change) => {
+          // console.log(
+          //   ">>>>>>>>>>>> change.type",
+          //   change.type,
+          //   change.newIndex,
+          //   change.doc.data()
+          // );
+
+          if (change.type === "added") {
+            if (change.doc.data().id) {
+              const newIndex = change.newIndex;
+              newShops.splice(newIndex, 0, change.doc.data());
+            }
+          }
+
+          if (change.type === "modified") {
+            if (change.doc.data().id) {
+              const newIndex = change.newIndex;
+              newShops.splice(newIndex, 1, change.doc.data());
+            }
+          }
+        });
+      }
+
+      dispatch(readShops(newShops));
+    });
+
+    if (payload.type === "delete") {
+      unsubscribe();
+    }
+  };
+};
+
+export const { createShop, updateShop, readShops } = shop.actions;
+export { fetchShops };
 export default shop;
