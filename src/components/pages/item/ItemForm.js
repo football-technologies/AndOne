2296ce7c5858d435f28d1596438da852d1a44096
imoto2000 <imoto.morefun@gmail.com>
2022-09-06 10:51:32 +0900

    add init ItemForm

--- src/components/pages/item/ItemForm.js
+++ src/components/pages/item/ItemForm.js
@@ -1,16 +1,17 @@
-import { query } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const ItemForm = () => {
   const router = useRouter();
 
-  console.log(">>>>>> init router", router);
   console.log(">>>>>> init router.query", router.query);
 
   useEffect(() => {
     console.log(">>>>>> useEffect", router.query);
-  }, []);
+    if (router.isReady) {
+      console.log(">>>>>> useEffect is Ready:", router.query);
+    }
+  }, [router.isReady]);
 
   return (
     <>
