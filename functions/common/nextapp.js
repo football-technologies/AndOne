2fd2386b2223bee9b97c8926f714b8eac3c68290
imoto2000 <imoto.morefun@gmail.com>
2022-09-14 09:04:50 +0900

    add basic use

--- functions/common/nextapp.js
+++ functions/common/nextapp.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const { default: next } = require("next");
 const nextConfig = require("../next.config.js");
+const express = require("express");
+const basicAuth = require("basic-auth-connect");
 
 const nextjsServer = next({
   ...nextConfig,
@@ -9,6 +11,14 @@ const nextjsServer = next({
     distDir: ".next",
   },
 });
+
+const app = express();
+// const USERNAME = functions.config().basic_auth.name;
+// const PASSWORD = functions.config().basic_auth.password
+const USERNAME = "imoto";
+const PASSWORD = "imoto";
+app.use(basicAuth(USERNAME, PASSWORD));
+
 const nextjsHandle = nextjsServer.getRequestHandler();
 
 const runtimeOpts = {
@@ -18,10 +28,20 @@ const runtimeOpts = {
   minInstances: 1,
 };
 
-module.exports = functions
-  .runWith(runtimeOpts)
-  .https.onRequest(async (req, res) => {
-    // exports.nextjsFunc = https.onRequest((req, res) => {
-    res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
-    return nextjsServer.prepare().then(() => nextjsHandle(req, res));
-  });
+app.use(async (req, res) => {
+  module.exports = functions
+    .runWith(runtimeOpts)
+    .https.onRequest(async (req, res) => {
+      // exports.nextjsFunc = https.onRequest((req, res) => {
+      res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
+      return nextjsServer.prepare().then(() => nextjsHandle(req, res));
+    });
+});
+
+// module.exports = functions
+//   .runWith(runtimeOpts)
+//   .https.onRequest(async (req, res) => {
+//     // exports.nextjsFunc = https.onRequest((req, res) => {
+//     res.set("Cache-Control", "public, max-age=600, s-maxage=1200");
+//     return nextjsServer.prepare().then(() => nextjsHandle(req, res));
+//   });
