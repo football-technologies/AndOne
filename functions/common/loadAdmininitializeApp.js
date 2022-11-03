36a4439e0517b0094e0707eb381e4e86b0c78f1b
mino2022 <mino@football-technologies.work>
2022-11-03 17:43:27 +0900

    test

--- functions/common/loadAdmininitializeApp.js
+++ functions/common/loadAdmininitializeApp.js
@@ -2,6 +2,11 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const serviceAccount = functions.config().service_account;
 
+console.log(
+  ">>>>>>>>> process.env.FIREBASE_CONFIG",
+  process.env.FIREBASE_CONFIG
+);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
